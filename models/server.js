ba30c4000d20371d1d2bf370e524ef3967e18a78
cbcrisel <cristhian7096@gmail.com>
2022-06-01 13:40:06 -0400

    port changed

--- models/server.js
+++ models/server.js
@@ -29,7 +29,7 @@ class Server{
     }
 
     start(){
-        this.app.listen(this.port,()=>{
+        this.app.listen(process.env.PORT || 5000,()=>{
             console.log(`Server on port ${this.port}`);
         });
     }
